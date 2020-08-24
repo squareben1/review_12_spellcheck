16365110ed011215cb7a99c7b646011b59c4ff86
Squareben1 <bengittins@live.com>
2020-08-24 11:39:00 +0100

    Extracted dictionary state

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -1,12 +1,15 @@
 class SpellChecker 
+  def initialize
+    @dictionary = ["correct"]
+  end
+
   def spellcheck(string)
-    dictionary = ["correct"]
     words = string.split(" ")
     checked_words = []
 
     words.each do |word|
       down_case_word = word.downcase
-      if !dictionary.include?(down_case_word)
+      if !@dictionary.include?(down_case_word)
         checked_words.push("~#{word}~")
       else
         checked_words.push(word)
